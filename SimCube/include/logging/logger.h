4b7cbb3814407b29152deded72b385eae10d7e9b
corey- <corey-@users.noreply.github.com>
2024-03-19 17:29:27 -0500

    Add logger

---
+++ SimCube/include/logging/logger.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <array>
+#include <format>
+#include <iostream>
+#include <string>
+
+struct LoggerInfo
+{
+    std::string SourceName;
+};
+
+enum class LogLevel : uint8_t
+{
+    INFO  = 0,
+    WARN  = 1,
+    ERROR = 2
+};
+
+template <typename... Args>
+void Log(const LoggerInfo &info, LogLevel level, std::string_view message, Args &&...args)
+{
+    static constexpr std::array LOG_LEVEL_NAMES{ "INFO", "WARN", "ERROR" };
+
+    std::cout << std::format("[{}] ", info.SourceName);
+    std::cout << std::format("{}: ", LOG_LEVEL_NAMES.at(static_cast<std::underlying_type_t<LogLevel>>(level)));
+    std::cout << std::vformat(message, std::make_format_args(args...)) << "\n";
+}
+
+template <typename... Args>
+void DevLog(const LoggerInfo &info, LogLevel level, std::string_view message, Args &&...args)
+{
+#ifdef DEV_LOGGING
+    Log(info, level, message, std::forward<Args>(args)...);
+#endif
+}
